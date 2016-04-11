f680a584bbeb531baf9893fe1e270e62f3eb9e4c
Halliax <tmsheneman@gmail.com>
2016-04-11 17:51:11 -0400

    restructured test plate code, outputs a dictionary of (x,y,z) tuples to block ID

--- plate_simulation.py
+++ plate_simulation.py
@@ -1,10 +1,45 @@
+"""
+Testing plate tectonics library
+"""
 import platec
+import numpy as np
 
-p = platec.create(3, 1000, 800,
-                      0.65, 60,
-                      0.02, 1000000,
-                      0.33, 2, 10)
-while platec.is_finished(p) == 0:
-    platec.step(p)
-hm = platec.get_heightmap(p)
-platec.destroy(p)
+
+def generate_heightmap(seed, width, height):
+    """Takes seed, width, height (other variables built in), returns a giant
+    shitty list instead of an actual heightmap"""
+
+    sea_level = 0.65
+    erosion_period = 60
+    folding_ratio = 0.02
+    aggr_overlap_abs = 1000000
+    aggr_overlap_rel = 0.33
+    cycle_count = 2
+    num_plates = 10
+
+    p = platec.create(seed, width, height, sea_level, erosion_period,
+                        folding_ratio, aggr_overlap_abs,
+                        aggr_overlap_rel, cycle_count, num_plates)
+    while platec.is_finished(p) == 0:
+        platec.step(p)
+
+    hm = platec.get_heightmap(p)
+    pm = platec.get_platesmap(p)
+
+    platec.destroy(p)
+    return hm,pm
+
+
+
+if __name__ == "__main__":
+    seed = 3
+    width = 100
+    height = 100
+
+    hmap,pmap = generate_heightmap(seed,width,height)
+    int_hmap = [int(round(h)) for h in hmap]
+
+    heightmap = np.reshape(int_hmap,(width,height))
+
+    height_dict = {(x,y,z):1 for y in range(h) for x, h in enumerate(row)
+                    for z, row in enumerate(heightmap)}
