71b7f551dbf4259de2dbfef3cd4b2101ffbcc42b
willem.thorbecke@students.olin.edu <willem.thorbecke@students.olin.edu>
2016-04-14 13:39:23 -0400

    adding fractal alg to correct place/spelling fractal correctly

--- graphics/config.py
+++ graphics/config.py
@@ -21,5 +21,5 @@ DIRECTIONS = [(1, 0, 0),
 CHUNK_SIZE = 16
 
 # Speeds are in meters per second
-WALKING_SPEED = 5.0
-FLYING_SPEED = 4.0
+WALKING_SPEED = 20.0
+FLYING_SPEED = 20.0
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import pyglet
 from graphics import Window, setup
 from worldgen import generate_heightmap
+from factal_height_map import Terrain
 
 def main():
-    world = generate_heightmap(7487670, 350, 350)
+    #world = generate_heightmap(7487670, 350, 350)
+    world = Terrain(4)
     window = Window(world=world, width=800, height=600, caption='PythonCraft', resizable=True)
     setup()
     pyglet.app.run()
