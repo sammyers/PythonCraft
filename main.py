4e2dead835055277c00852196bef8f8d699649f4
willem.thorbecke@students.olin.edu <willem.thorbecke@students.olin.edu>
2016-04-14 14:00:17 -0400

    deleted factal alg

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import pyglet
 from graphics import Window, setup
 from worldgen import generate_heightmap
-from factal_height_map import Terrain
+from worldgen import fractal_height_map
 
 def main():
     #world = generate_heightmap(7487670, 350, 350)
-    world = Terrain(4)
+    world = fractal_height_map.Terrain(7)
     window = Window(world=world, width=800, height=600, caption='PythonCraft', resizable=True)
     setup()
     pyglet.app.run()
--- worldgen/fractal_height_map.py
+++ worldgen/fractal_height_map.py
@@ -1,20 +1,14 @@
 import numpy as np
 import math
 import random
-import os
-
-
-
-
-
 
 
 def Terrain(detail):
 	"""
 	initializes a height map with a size specified by 'detail'
+	returns a dictionary containing x, y and z values 
 	"""
 	
-
 	#size defines the length and width of a height map
 	size = math.pow(2,detail)+1
 	max_length = int(size)
@@ -35,6 +29,7 @@ def Terrain(detail):
 
 	print big_mama
 
+	#places height map into a dictionary
 	height_dict = {}
 	for z, row in enumerate(big_mama):
 		for x, h in enumerate(row):
@@ -43,48 +38,35 @@ def Terrain(detail):
 
 	return height_dict
 
-	
-
-	# print big_mama
-	# mlab.surf(big_mama)
-	# mlab.show()
-	
-
-	# yv = np.meshgrid(x, y, sparse=True) 
-	# np.plot(yv)
-
 
 def divide(max_length, size, b_m):
 
-	#some roughness constant...
-	roughness = .5
+	"""
+	divides the grid into squares and diamonds recursively in that order  
+	"""
+
+	roughness = .5 #dictates roughness of 
+
 	half = int(size/2)
-	x = int(size/2)
-	y = int(size/2)
-
-
 
 	scale = (roughness*size)/2
 
+	#ends recursion once size = 1
 	if half < 1:
 		return
 
+	#finds all the points that need a square part of algorithm applied to it
 	for y in range(half, max_length, size):
 		for x in range(half, max_length, size):
 			square(x, y, b_m, random.random()*scale*2-scale, half)
 		
-
+	#finds all the points that need diamond part of algorithm applied to it
 	for y in range(0, max_length, half):
-		for x in range((y + half)%(size+1), max_length, size):
-		
+		for x in range((y + half)%(size), max_length, size):
 			diamond(x, y, b_m, random.random()*scale*2-scale, half, max_length)
 		
-
+	#calls divide again to increase detail 	
 	divide(max_length,int(size/2), b_m)
-
-
-
-
 
 
 def diamond(x, y, b_m, offset, size, ml):
@@ -92,53 +74,41 @@ def diamond(x, y, b_m, offset, size, ml):
 	takes average of four surrounding points and sets that as the base height for the center point
 	then adds a random offset value to the center point
 	"""
+
 	avg = []
 
-	print 'x ', x
-	print 'y ',y
+	# print 'x ', x
+	# print 'y ',y
+
 	if x - size >= 0:
 		avg.append(b_m[x-size,y])
-
 	if y - size >= 0:
 		avg.append(b_m[x,y-size])
 	if x + size < ml:
 		avg.append(b_m[x+size,y])
 	if y + size < ml:
 		avg.append(b_m[x,y+size])
-	print avg
+	#print avg
 
 	avgg = np.average(avg)
-	print avgg
-
-	# try:
-	# 	avg = np.average([b_m[x-size,y], b_m[x,y-size], b_m[x+size,y], b_m[x,y+size]])
-	# except IndexError:
-	# 	print 'size, ', size
-	# 	print 'x ', x
-	# 	print 'y ', y
-	# 	avg = size   
-	# 	print 'checkx, +', x + size
-	# 	print 'checky, +', y + size
-	# 	print 'checkx, -', x - size
-	# 	print 'checky -', y-size
-	# 	print 'height_check, ', avg + offset
 	
 
-	b_m[x,y] = avgg + 0 #offset
+	b_m[x,y] = avgg + offset
 	if x == 2 and y == 1:
 		b_m[x,y] = 10
 
 
 def square(x, y, b_m, offset, size):
 
+	"""
+	computes the average height of the four surrounding points and adds and additional random height to 
+	average
+	"""
+
 	avg = np.average([b_m[x-size,y-size], b_m[x+size,y-size], b_m[x-size,y+size], b_m[x+size,y+size]])
 
-	b_m[x,y] = avg + 0 #offset
-
-
-#Terrain(4)
-
+	b_m[x,y] = avg + offset
 
 
 if __name__ == "__main__":
- 	Terrain(2)
+ 	Terrain(3)
