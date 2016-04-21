5bec1dc21ec41a40095d5afb8b0873ae2a534835
Halliax <tmsheneman@gmail.com>
2016-04-21 15:51:09 -0400

    Added technical review 2 background doc

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from worldgen import generate_heightmap
 from worldgen import fractal_height_map
 
 def main():
-    #world = generate_heightmap(7487670, 350, 350)
-    world = fractal_height_map.Terrain(7)
+    world = generate_heightmap(7487670, 350, 350)
+    # world = fractal_height_map.Terrain(7)
     window = Window(world=world, width=800, height=600, caption='PythonCraft', resizable=True)
     setup()
     pyglet.app.run()
