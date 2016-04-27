b482691611380f0c802c9a9a5c973deef5b000b1
Halliax <tmsheneman@gmail.com>
2016-04-27 01:28:28 -0400

    Wrote most of the erosion simulation functions. Modified fractal heightmap output for testing erosion

--- worldgen/erosion.py
+++ worldgen/erosion.py
@@ -8,34 +8,40 @@ Mei, et al. Fast Hydraulic Erosion Simulation and Visualization on GPU
 
 import math as mfn
 import numpy as np
+from fractal_height_map import terrain
 
 
 class Cell(object):
     """Represents a cell in the erosion grid. Tracks relevant attributes.
 
-    attributes: t_height, w_height, sediment, fL, fR, fT, fB (outflow
-    fluxes), velocity"""
+    attributes: b (terrain height), d (water height), s (sediment), fL, fR, fT, fB
+    (outflow fluxes), velocity
+    temp attributes: d1, d2, s1"""
 
     def __init__(self, z):
-        self.t_height = z
-        self.w_height = 0
-        self.sediment = 0
+        self.b = z
+        self.d = 0
+        self.s = 0
         self.fL = 0
         self.fR = 0
         self.fT = 0
         self.fB = 0
         self.velocity = [0,0]
 
+        self.d1 = 0
+        self.d2 = 0
+        self.s1 = 0
+
     def __repr__(self):
-        return ("Cell(t_height=%r,w_height=%r,sediment=%r,fL=%r,fR=%r,fT=%r,fB=%r,velocity=%r)"
-        %(self.t_height,self.w_height,self.sediment,self.fL,self.fR,self.fT,
+        return ("Cell(b=%r,d=%r,s=%r,fL=%r,fR=%r,fT=%r,fB=%r,velocity=%r)"
+        %(self.b,self.d,self.s,self.fL,self.fR,self.fT,
         self.fB,self.velocity))
 
 
 class CellGrid(np.ndarray):
     """Grid of Cells with relevant functions. Represents the simulation surface.
 
-    attributes: """
+    attributes: numpy subclass"""
 
     def __new__(cls, data):
         cells = [[Cell(z) for z in row] for row in data]
@@ -45,24 +51,127 @@ class CellGrid(np.ndarray):
     def __array_finalize__(self,obj):
         if obj is None: return
 
-    def water_increment(dt):
+    def water_increment(self,(x,y),waterRate,tstep):
+        # update temp water level with added water
+        self[x,y].d1 = self[x,y].d + waterRate * tstep
+
+    def flow_simulation(self,(x,y),tstep):
+        A = 0.785 # cross-sectional area of virtual "pipe"
+
+        # calculate total height differences with adjacent cells
+        hL = self[x,y].b + self[x,y].d1 - self[x-1,y].b - self[x-1,y].d1
+        hR = self[x,y].b + self[x,y].d1 - self[x+1,y].b - self[x+1,y].d1
+        hB = self[x,y].b + self[x,y].d1 - self[x,y-1].b - self[x,y-1].d1
+        hT = self[x,y].b + self[x,y].d1 - self[x,y+1].b - self[x,y+1].d1
+
+        # fluxes outwards to adjacent cells (note: coordinate system is rotated)
+        self[x,y].fL = max(0, self[x,y].fL + tstep * A * 9.81 * hL)
+        self[x,y].fR = max(0, self[x,y].fR + tstep * A * 9.81 * hR)
+        self[x,y].fB = max(0, self[x,y].fB + tstep * A * 9.81 * hB)
+        self[x,y].fT = max(0, self[x,y].fT + tstep * A * 9.81 * hT)
+
+        # total flux, calculate k constant to prevent negative water values
+        fTotal = self[x,y].fL + self[x,y].fR + self[x,y].fB + self[x,y].fT
+        if  fTotal != 0:
+            k = min(1, self[x,y].d1 / (fTotal * tstep))
+        else:
+            k = 1
+
+        self[x,y].fL *= k
+        self[x,y].fR *= k
+        self[x,y].fB *= k
+        self[x,y].fT *= k
+
+        # change in water level based on total flux in and out
+        total_fin = self[x-1,y].fR +self[x+1,y].fL +self[x,y-1].fT +self[x,y+1].fB
+        total_fout = self[x,y].fL +self[x,y].fR +self[x,y].fB +self[x,y].fT
+        deltaVolume = tstep * (total_fin - total_fout)
+
+        # update new temp water level, find average of start/end levels
+        self[x,y].d2 = self[x,y].d1 + deltaVolume
+        dBar = (self[x,y].d1 + self[x,y].d2) / 2
+
+        # find average flux/direction of water passing through cell
+        avFluxX = (self[x-1,y].fR -self[x,y].fL +self[x,y].fR -self[x+1,y].fL) / 2
+        avFluxY = (self[x,y-1].fT -self[x,y].fB +self[x,y].fT -self[x,y+1].fB) / 2
+
+        # find velocity vector based on flux and average water level
+        if avFluxX != 0:
+            u = avFluxX / dBar
+        else:
+            u = 0
+        if avFluxY != 0:
+            v = avFluxY / dBar
+        else:
+            v = 0
+
+        # update local velocity vector
+        self[x,y].velocity = [u,v]
+
+
+    def erosion_deposition(self,(x,y),tstep):
+        kc = 1 # sediment capacity constant
+        ks = 0.5 # dissolving constant
+        kd = 0.3 # deposition constant
+
+        # find local tilt angle by averaging absolutes of opposite pairs of angles
+        alpha = (abs(mfn.atan(self[x,y].b - self[x-1,y].b) - mfn.atan(self[x,y].b
+        - self[x+1,y].b)) + abs(mfn.atan(self[x,y].b - self[x,y-1].b) -
+        mfn.atan(self[x,y].b - self[x,y+1].b))) / 4
+
+        # threshold of tilt before no sediment is picked up
+        if alpha < 0.1:
+            alpha = 0
+
+        # carrying capacity based on tilt angle and velocity field
+        capacity = kc * mfn.sin(alpha) * np.linalg.norm(self[x,y].velocity)
+
+        # pick up sediment and add to suspended sediment with excess capacity
+        if capacity > self[x,y].s:
+            self[x,y].b = self[x,y].b - ks * (capacity - self[x,y].s)
+            self[x,y].s1 = self[x,y].s + ks * (capacity - self[x,y].s)
+
+        # deposit sediment if at or exceeding capacity
+        if capacity <= self[x,y].s:
+            self[x,y].b = self[x,y].b + kd * (self[x,y].s - capacity)
+            self[x,y].s1 = self[x,y].s - kd * (self[x,y].s - capacity)
+
+
+    def sediment_transportation(self,s1,vtt):
         pass
 
-    def flow_simulation(d1,bt,ft):
-        pass
-
-    def erosion_deposition(vtt,bt,st):
-        pass
-
-    def sediment_transportation(s1,vtt):
-        pass
-
-    def evaporation(d2):
+    def evaporation(self,d2):
         pass
 
 
 if __name__ == "__main__":
 
-    heightmap = np.array([[1,2,3],[4,5,6]])
-    terrain = CellGrid(heightmap)
-    print terrain
+    heightmap = terrain(5)
+    step = 1
+    t = CellGrid(heightmap)
+    t.water_increment((3,4),4.0,step)
+    t.flow_simulation((3,4),step)
+    t.flow_simulation((2,3),step)
+    t.flow_simulation((2,4),step)
+    t.flow_simulation((2,5),step)
+    t.flow_simulation((3,3),step)
+    t.flow_simulation((3,5),step)
+    t.flow_simulation((4,3),step)
+    t.flow_simulation((4,4),step)
+    t.flow_simulation((4,5),step)
+    print t[2,3].b, t[2,4].b, t[2,5].b
+    print t[3,3].b, t[3,4].b, t[3,5].b
+    print t[4,3].b, t[4,4].b, t[4,5].b
+    print t[3,4].velocity
+    t.erosion_deposition((3,4),step)
+    t.erosion_deposition((2,3),step)
+    t.erosion_deposition((2,4),step)
+    t.erosion_deposition((2,5),step)
+    t.erosion_deposition((3,3),step)
+    t.erosion_deposition((3,5),step)
+    t.erosion_deposition((4,3),step)
+    t.erosion_deposition((4,4),step)
+    t.erosion_deposition((4,5),step)
+    print t[2,3].s1, t[2,4].s1, t[2,5].s1
+    print t[3,3].s1, t[3,4].s1, t[3,5].s1
+    print t[4,3].s1, t[4,4].s1, t[4,5].s1
--- worldgen/fractal_height_map.py
+++ worldgen/fractal_height_map.py
@@ -6,20 +6,20 @@ import math
 import random
 
 
-def Terrain(detail):
+def terrain(detail):
 	"""
 	initializes a height map with a size specified by 'detail'
-	returns a dictionary containing x, y and z values 
+	returns a dictionary containing x, y and z values
 	"""
-	
+
 	#size defines the length and width of a height map
 	size = math.pow(2,detail)+1
 	max_length = int(size)
-	
+
 	#defines height map
 	big_mama = np.zeros((size,size), np.int32)
 
-	
+
 	#defines all the corners to be halfway up the height map
 	big_mama[0,0] = size/2
 	big_mama[0,size-1] = size/2
@@ -39,16 +39,17 @@ def Terrain(detail):
 			for y in range(h + 1):
 				height_dict[(x, y, z)] = 6 if h == 0 else (1 if y == h else 2)
 
-	return height_dict
+	# return height_dict
+	return big_mama
 
 
 def divide(max_length, size, b_m):
 
 	"""
-	divides the grid into squares and diamonds recursively in that order  
+	divides the grid into squares and diamonds recursively in that order
 	"""
 
-	roughness = .5 #dictates roughness of 
+	roughness = .5 #dictates roughness of
 
 	half = int(size/2)
 
@@ -62,13 +63,13 @@ def divide(max_length, size, b_m):
 	for y in range(half, max_length, size):
 		for x in range(half, max_length, size):
 			square(x, y, b_m, random.random()*scale*2-scale, half)
-		
+
 	#finds all the points that need diamond part of algorithm applied to it
 	for y in range(0, max_length, half):
 		for x in range((y + half)%(size), max_length, size):
 			diamond(x, y, b_m, random.random()*scale*2-scale, half, max_length)
-		
-	#calls divide again to increase detail 	
+
+	#calls divide again to increase detail
 	divide(max_length,int(size/2), b_m)
 
 
@@ -94,7 +95,7 @@ def diamond(x, y, b_m, offset, size, ml):
 	#print avg
 
 	avgg = np.average(avg)
-	
+
 
 	b_m[x,y] = avgg + offset
 	if x == 2 and y == 1:
@@ -104,7 +105,7 @@ def diamond(x, y, b_m, offset, size, ml):
 def square(x, y, b_m, offset, size):
 
 	"""
-	computes the average height of the four surrounding points and adds and additional random height to 
+	computes the average height of the four surrounding points and adds and additional random height to
 	average
 	"""
 
@@ -114,4 +115,4 @@ def square(x, y, b_m, offset, size):
 
 
 if __name__ == "__main__":
- 	Terrain(3)
+ 	terrain(3)
